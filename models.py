eb923d78c2f7659df0b272c9b340862268e0ef6e
Ady <polcsek.dm90@gmail.com>
2021-05-22 21:16:46 -0700

    added models to the database

---
+++ models.py
@@ -0,0 +1,33 @@
+from app import db, myApp, login_manager
+from datetime import datetime
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    hashed_password = db.Column(db.String(32), index=True, nullable=False)
+    joined_time = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    user_post = db.relationship('Post', backref='user', lazy=True)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+    
+    def hash_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), index=True, nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    text = db.Column(db.Text, index=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
