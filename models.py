fa240661bbff60f7d7a65e8be2a4ebf2a538b96b
Ady <polcsek.dm90@gmail.com>
2021-05-24 22:52:58 -0700

    added cascade on relationships

--- models.py
+++ models.py
@@ -11,23 +11,42 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
+
     username = db.Column(db.String(20), index=True, unique=True, nullable=False)
+
     email = db.Column(db.String(64), index=True, unique=True, nullable=False)
+
     hashed_password = db.Column(db.String(32), index=True, nullable=False)
+
     joined_time = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    user_post = db.relationship('Post', backref='user', lazy=True)
+
+    user_post = db.relationship('Post', backref='user', cascade='all, delete, delete-orphan', lazy=True)
 
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
     
     def hash_password(self, password):
         self.hashed_password = generate_password_hash(password)
+    
+    def __repr__(self):
+        return 'User {}\nusername:_{}\nemail:_{}'.format(self.id, self.username, self.email)
 
 
 class Post(db.Model):
+    __tablename__ = 'post'
+
     id = db.Column(db.Integer, primary_key=True)
+
     title = db.Column(db.String(80), index=True, nullable=False)
+
     date_posted = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
     text = db.Column(db.Text, index=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='cascade'), nullable=False)
+
+    def __repr__(self):
+        return 'Post {}\n{}\n{}'.format(self.id, self.title, self.date_posted)
